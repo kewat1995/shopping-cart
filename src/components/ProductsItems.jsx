d1fddf7d6362031cb86306f93fc778805a32e445
kewat1995 <surendra121030@gmail.com>
2024-02-08 14:42:27 +0000

    yes

--- src/components/ProductsItems.jsx
+++ src/components/ProductsItems.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+/* eslint-disable react/prop-types */
+
 import { useDispatch, useSelector } from "react-redux";
 import { cartAction } from "../store/shopingCart";
 const ProductsItems = ({ items }) => {
